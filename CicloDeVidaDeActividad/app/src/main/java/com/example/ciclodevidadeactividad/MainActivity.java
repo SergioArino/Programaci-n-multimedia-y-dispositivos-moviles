5e4f466d55010a3aef9cb4159bb562aa53ae2a6b
Sergio <sergio_aperez@alumnos-santagema.es>
2025-09-29 20:38:50 +0200

    Nueva tarea CicloDeVidaActividad

---
+++ CicloDeVidaDeActividad/app/src/main/java/com/example/ciclodevidadeactividad/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ciclodevidadeactividad;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Log.i("ejemplo", "Estoy en on create" );
+        }
+        protected void onStart(){
+            super.onStart();
+            Log.i("ejemplo", "Estoy en on Start" );
+        };
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("ejemplo", "Estoy en on Restart" );
+    };
+    protected void onResume(){
+        super.onResume();
+        Log.i("ejemplo", "Estoy en on Resume" );
+    };
+    protected void onPause(){
+        super.onPause();
+        Log.i("ejemplo", "Estoy en on Pause" );
+    };
+    protected void onStop(){
+        super.onStop();
+        Log.i("ejemplo", "Estoy en on Stop" );
+    };
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("ejemplo", "Estoy en on Destroy" );
+    };
+    }
