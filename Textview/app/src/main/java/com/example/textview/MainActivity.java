9fb74ad304f4be131724be15b2b966152a9f2e77
Sergio <sergio_aperez@alumnos-santagema.es>
2025-10-07 20:39:11 +0200

    Textview

---
+++ Textview/app/src/main/java/com/example/textview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.textview;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    protected void onStart(){
+        super.onStart();
+        TextView miTexto = (TextView) findViewById(R.id.texto);
+        miTexto.setText("Nuevo texto");
+        miTexto.setTextColor(Color.parseColor("#0000FF"));
+        miTexto.setTextColor(Color.RED);
+        miTexto.setTypeface(null, Typeface.ITALIC);
+        miTexto.setTextSize(24);
+        miTexto.setTypeface(Typeface.SANS_SERIF);
+    }
+}
