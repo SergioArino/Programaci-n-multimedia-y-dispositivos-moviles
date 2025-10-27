9f121679ffa7b2944c46b0928c6561f7b8d2b15b
Sergio <sergio_aperez@alumnos-santagema.es>
2025-10-27 20:48:11 +0100

    Textoconopciones

--- EditText/app/src/main/java/com/example/edittext/MainActivity.java
+++ EditText/app/src/main/java/com/example/edittext/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.edittext;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        String [] opciones = {"opcion1","opcion2","opcion3","opcion4","opcion5"};
+        AutoCompleteTextView textoleido = findViewById(R.id.miTexto);
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line,opciones);
+        textoleido.setAdapter(adaptador);
     }
 }
